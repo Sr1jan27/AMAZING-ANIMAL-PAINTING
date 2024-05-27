f258e2359540e149cef924e6d34c381db800da8c
srijan_singh <singhsrijan809@gmail.com>
2024-05-27 23:07:39 +0530

    removal of the already present html code and some refactoring

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductListComponent } from './product/product-list/product-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/products', pathMatch: 'full'},
+  {path: 'products', component: ProductListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
